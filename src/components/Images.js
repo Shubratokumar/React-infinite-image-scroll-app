5a971500f07671b7bc4e39cb5760056193989d08
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-08-18 09:06:16 +0600

    initial setup

---
+++ src/components/Images.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Images = () => {
+    return (
+        <div>
+            Images here
+        </div>
+    );
+};
+
+export default Images;
---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div>
+            Hello from loader.
+        </div>
+    );
+};
+
+export default Loader;
